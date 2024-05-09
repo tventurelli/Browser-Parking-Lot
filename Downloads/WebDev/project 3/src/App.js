cba9062de3973a0964322254a195209cc1aa1ebc
unknown <trevorventurelli@gmail.com>
2024-05-09 13:35:50 -0700

    tagline

--- Downloads/WebDev/project 3/src/App.js
+++ Downloads/WebDev/project 3/src/App.js
@@ -6,6 +6,7 @@ function App() {
       <header className="App-header">
           <h1>Browser Parking Lot</h1>
           <p>Send most of your browser taps into retirement</p>
+          <p>Second Tag</p>
       </header>
     </div>
   );
